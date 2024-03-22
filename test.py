b2d852790c61becbe2be83df865be1bc4f93b4d9
Yubo Huang <81608668+sawyercharlton@users.noreply.github.com>
2024-03-22 14:22:55 -0400

    Update test.py

--- test.py
+++ test.py
@@ -3,19 +3,17 @@ from PIL import Image
 from numpy import asarray
 
 image = Image.open('./0.jpg')
-image1 = Image.open('./1.jpg')
 img_np = asarray(image)
 img_np_flat = img_np.flatten()
-img_np1 = asarray(image1)
-img_np_flat1 = img_np1.flatten()
 print("pixels: ", img_np_flat.tolist())
 print("len(img_np_flat): ", len(img_np_flat))
 tokenizer = BasicTokenizer()
 
 text = img_np_flat
-text1 = img_np_flat1
 tokenizer.train(text, 256 + 48,  verbose=True)  # 256 are the byte tokens, then do 48 merges
+# tokenizer.load('toy.model')  # load trained model
 output_encoder = tokenizer.encode(text)
+
 print("output_encoder: ", output_encoder)
 print("len(output_encoder): ", len(output_encoder))
 
